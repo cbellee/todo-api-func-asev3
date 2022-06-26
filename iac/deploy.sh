7955137c5bf085305c6c3a3218d47ab6c10adc36
Chris Bellee <8634922+cbellee@users.noreply.github.com>
2022-06-26 18:35:41 +1000

    added dot-sourced .env

--- iac/deploy.sh
+++ iac/deploy.sh
@@ -2,14 +2,22 @@ zipName=funcapp.zip
 location='australiaeast'
 appName='todo-api'
 rgName="$appName-func-rg"
-adminObjectId='57963f10-818b-406d-a2f6-6e758d86e259'
-sqlAdminUserPassword='M1cr0soft1234567890'
+adminObjectId='57963f10-818b-406d-a2f6-6e758d86e259' # change to your user objectID
+
+# create a file named '.env' in the same directory as this script 
+# add the line below to the file 
+# sqlAdminUserPassword=<your password> 
+
+# dot-source the ./.env file to load the password as an env var
+. ./.env
 
 # compile app
 cd ../api
 go build -o handler
 
 # compress funtion app code to .zip file
+# you'll need to install the 'zip' package first
+# $ apt install zip
 zip -r ../iac/$zipName *
 
 cd ../iac
